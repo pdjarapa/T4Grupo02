8b9099af189b30db9579960b53b03419419bc4dc
Paulo <paulo.jara@unl.edu.ec>
2022-10-28 01:45:35 -0500

    add codigo models

--- gt04pares/admin.py
+++ gt04pares/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from gt04pares.models import *
 
 # Register your models here.
+admin.site.register(Sesion)
+admin.site.register(Tutoria)
--- gt04pares/models.py
+++ gt04pares/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Recurso(models.Model):
+    title = models.CharField(max_length=50)
+    title_minimo = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+class Sesion(Recurso):
+    description = models.TextField()
+
+    def __str__(self):
+        """
+        """
+        return "Video: %s - %s " % (self.title, self.title_minimo)
+
+class Tutoria(Recurso):
+    markdown_content = models.TextField()
+
+    def __str__(self):
+        """
+        """
+        return "Video: %s - %s " % (self.title, self.title_minimo)
